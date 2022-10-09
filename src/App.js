2b344d9bd9aae2fe5bfe9704557dee2a07371a64
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-10-09 17:58:02 -0400

    Removed logo, added example code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
-import Button from '@mui/material/Button';
+import {Container, Paper,Typography} from '@mui/material';
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  const styles = {
+    Container: {
+      height:'100vh',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    FlexBox:{
+      display:'flex',
+      flexDirection:'column',
+      gap:2,
+      padding:5
+    }
+  }
+  return (
+    <Container sx={styles.Container}>
+      <Paper sx={styles.FlexBox} elevation={10}>
+        <Typography variant="h1">Naked React with MUI</Typography>
+        <Typography variant="h3">React without the crap!</Typography>
+        <code><a target="_blank" href="https://digyt.co">Made with love by Digyt</a></code>
+      </Paper>
+    </Container>
+  );
 }
 
 export default App;
